d285fedf22da18f802ee73abf92ab8966baaadd4
Simon Janssen <simon.janssen@tum.de>
2020-03-24 18:44:44 +0100

    lab3 - update run.py for submission

--- lab3_theodor_husefest_simon_janssen/run.py
+++ lab3_theodor_husefest_simon_janssen/run.py
@@ -2,6 +2,8 @@
 import data_utils as du
 import argparse
 import numpy as np
+import torch
+from models import ConvNet
 
 #########################################################################
 # TODO:                                                                 #
@@ -10,6 +12,7 @@ import numpy as np
 # Note: The input to the function is similar to the output of the method#
 # "get_CIFAR10_data" found in the notebooks.                            #
 #########################################################################
+
 
 def predict_usingCNN(X):
     #########################################################################
@@ -20,7 +23,25 @@ def predict_usingCNN(X):
     # Note: For the predictions, you have to return the index of the max    #
     # value                                                                 #
     #########################################################################
-    pass
+
+    # load network
+    fp_checkpoint = 'model_0.740.ckpt'
+    net = ConvNet()
+    net.load_state_dict(torch.load(fp_checkpoint))
+
+    # init dataloader
+    x = torch.from_numpy(X).float()
+    test_dataset = torch.utils.data.TensorDataset(x)
+    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=64)
+
+    # evaluate network
+    net.eval()
+    y_pred = np.empty(0)
+    with torch.no_grad():
+        for data in test_dataloader:
+            images = data[0]
+            y_pred_batch = torch.argmax(net.predict(images), dim=1).numpy()
+            y_pred = np.append(y_pred, y_pred_batch)
     #########################################################################
     #                       END OF YOUR CODE                                #
     #########################################################################
@@ -28,17 +49,21 @@ def predict_usingCNN(X):
    
 
 def main(filename, group_number):
-    X,Y = du.load_CIFAR_batch(filename)
+    X, Y = du.load_CIFAR_batch(filename)
+
+    X, Y = X[:100, :, :, :], Y[:100]  # TODO: remove, only for debugging to avoid memory overflow
+
     mean_pytorch = np.array([0.4914, 0.4822, 0.4465])
     std_pytorch = np.array([0.2023, 0.1994, 0.2010])
     X_pytorch = np.divide(np.subtract( X/255 , mean_pytorch[:,np.newaxis,np.newaxis]), std_pytorch[:,np.newaxis,np.newaxis])
     prediction_cnn = predict_usingCNN(X)
     acc_cnn = sum(prediction_cnn == Y)/len(X)
-    print("Group %s ... CNN= %f"%(group_number, acc_cnn))
-    
+    print("Group %s ... CNN= %f" % (group_number, acc_cnn))
+
+
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
-    ap.add_argument("-t", "--test", required=True, help="path to test file")
-    ap.add_argument("-g", "--group", required=True, help="group number")
+    ap.add_argument("-t", "--test", default="data/cifar-10-batches-py/test_batch", required=False, help="path to test file")
+    ap.add_argument("-g", "--group", default="Theodor and Simon", required=False, help="group number")
     args = vars(ap.parse_args())
-    main(args["test"],args["group"])
+    main(args["test"], args["group"])
