e2baf1e1ddf57d43657d8e2b44f9985185392574
kminoda <koji.m.minoda@gmail.com>
2020-05-21 10:17:22 +0900

    edit args for exec_yolo.sh

--- project/milestone3/README.md
+++ project/milestone3/README.md
@@ -3,6 +3,6 @@
 ### Execute Object Detection
 Just run the following command.  
 ```
-$ source exec_yolo.sh  
+$ source exec_yolo.sh /path/to/video /path/to/workdir /path/to/weight 
 ```
 NOTE: Change the first three lines for your configuration.
--- project/milestone3/exec_yolo.sh
+++ project/milestone3/exec_yolo.sh
@@ -1,7 +1,9 @@
 # Change these three variables for your configuration
 video_path=$1 # /home/minoda/koji_hdd/datasets/DLAV_videos/MOT16-10-raw.webm
-save_folder=$2 # /home/minoda/git/DLAV/project/milestone3/images
-out_folder=$3 # /home/minoda/git/DLAV/project/milestone3/yolov3_outputs
+workdir=$2 
+weight_path=$3
+save_folder=${workdir}/images
+out_folder=${workdir}/yolov3_outputs
 
 python3 extract_images_from_video.py ${video_path} ${save_folder}
 
@@ -9,6 +11,6 @@ python3 ../PyTorch-YOLOv3/generate_person_images.py \
         --image_folder ${save_folder} \
         --outputs_folder ${out_folder} \
         --model_def ../PyTorch-YOLOv3/config/yolov3-custom.cfg \
-        --weights_path ../PyTorch-YOLOv3/weights/yolov3_300420.pth \
+        --weights_path ${weight_path} \
         --class_path ../PyTorch-YOLOv3/data/ecp/ecp.names \
         --save_thres 0.99
