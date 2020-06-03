51d71058116de1deba51f806c8bcbf67ad34c1ef
kminoda <koji.m.minoda@gmail.com>
2020-06-03 10:48:27 +0900

    erased commentouts

--- project/milestone3/milestone3.sh
+++ project/milestone3/milestone3.sh
@@ -4,29 +4,29 @@ video_name=$2
 path_to_weight_YOLOv3=/home/minoda/yolov3_180520.pth
 path_to_weight_ABDNet=/home/minoda/msmt17_final_best.pth.zip.tar
 
-# rm -rf outputs/$2
+rm -rf outputs/$2
 
-# # video -> images
-# echo "\n######## Generate images from video ########"
-# python3 ./utils/extract_images_from_video.py ${video_path} outputs/$2/images
+# video -> images
+echo "\n######## Generate images from video ########"
+python3 ./utils/extract_images_from_video.py ${video_path} outputs/$2/images
 
-# # Perform YOLOv3 for pedestrian detection
-# echo "\n######## Start Object Detection by YOLOv3 ########"
-# python3 ../PyTorch-YOLOv3/generate_person_images.py \
-#         --image_folder outputs/$2/images \
-#         --outputs_folder outputs/$2/yolov3_outputs \
-#         --model_def ../PyTorch-YOLOv3/config/yolov3-custom.cfg \
-#         --weights_path ${path_to_weight_YOLOv3} \
-#         --class_path ../PyTorch-YOLOv3/data/ecp/ecp.names \
-#         --save_thres 0.99
+# Perform YOLOv3 for pedestrian detection
+echo "\n######## Start Object Detection by YOLOv3 ########"
+python3 ../PyTorch-YOLOv3/generate_person_images.py \
+        --image_folder outputs/$2/images \
+        --outputs_folder outputs/$2/yolov3_outputs \
+        --model_def ../PyTorch-YOLOv3/config/yolov3-custom.cfg \
+        --weights_path ${path_to_weight_YOLOv3} \
+        --class_path ../PyTorch-YOLOv3/data/ecp/ecp.names \
+        --save_thres 0.99
 
-# # Perform re-ID
-# echo "\n######## Start re-ID by ABD-Net ########"
-# python3 ../ABD-Net/classify.py -t pedestrianreid -s not_used \
-#         --load-weights=${path_to_weight_ABDNet} \
-#         --root=./outputs \
-#         --video=${video_name} \
-#         --save-dir=output
+# Perform re-ID
+echo "\n######## Start re-ID by ABD-Net ########"
+python3 ../ABD-Net/classify.py -t pedestrianreid -s not_used \
+        --load-weights=${path_to_weight_ABDNet} \
+        --root=./outputs \
+        --video=${video_name} \
+        --save-dir=output
 
 
 # Generate deliberative
