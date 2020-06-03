db08166330e3512b8ff53ec62b11bca341b2b95d
kminoda <koji.m.minoda@gmail.com>
2020-06-03 10:37:38 +0900

    finalized paths, except ABD-Net

--- project/milestone3/milestone3.sh
+++ project/milestone3/milestone3.sh
@@ -1,36 +1,35 @@
 video_path=$1 # /home/minoda/koji_hdd/datasets/DLAV_videos/MOT16-10-raw.webm
 video_name=$2
 
-save_folder=./intermediates/images
-pedestrians_folder=./intermediates/yolov3_outputs
-path_to_weight_YOLOv3=../PyTorch-YOLOv3/weights/yolov3_300420.pth
-path_to_weight_ABDNet=../ABDNet/checkpoints/final_model.zip.tar
+path_to_weight_YOLOv3=/home/minoda/yolov3_180520.pth
+path_to_weight_ABDNet=/home/minoda/msmt17_final_best.pth.zip.tar
 
-rm -rf ${save_folder}
-rm -rf ${pedestrians_folder}
+# rm -rf outputs/$2
 
-# video -> images
-python3 ./utils/extract_images_from_video.py ${video_path} ${save_folder}
+# # video -> images
+# echo "\n######## Generate images from video ########"
+# python3 ./utils/extract_images_from_video.py ${video_path} outputs/$2/images
 
-# Perform YOLOv3 for pedestrian detection
-python3 ../PyTorch-YOLOv3/generate_person_images.py \
-        --image_folder ${save_folder} \
-        --outputs_folder ${pedestrians_folder} \
-        --model_def ../PyTorch-YOLOv3/config/yolov3-custom.cfg \
-        --weights_path ${path_to_weight_YOLOv3} \
-        --class_path ../PyTorch-YOLOv3/data/ecp/ecp.names \
-        --save_thres 0.95
+# # Perform YOLOv3 for pedestrian detection
+# echo "\n######## Start Object Detection by YOLOv3 ########"
+# python3 ../PyTorch-YOLOv3/generate_person_images.py \
+#         --image_folder outputs/$2/images \
+#         --outputs_folder outputs/$2/yolov3_outputs \
+#         --model_def ../PyTorch-YOLOv3/config/yolov3-custom.cfg \
+#         --weights_path ${path_to_weight_YOLOv3} \
+#         --class_path ../PyTorch-YOLOv3/data/ecp/ecp.names \
+#         --save_thres 0.99
 
-# Perform re-ID
-# python ABDNet/inference.py
-
-python3 classify.py -t pedestrianreid -s not_used \
-        --load-weights=${path_to_weight_ABDNet} \
-        --root=data/milestone3 \
-        --video=${video_name} \
-        --save-dir=output
+# # Perform re-ID
+# echo "\n######## Start re-ID by ABD-Net ########"
+# python3 ../ABD-Net/classify.py -t pedestrianreid -s not_used \
+#         --load-weights=${path_to_weight_ABDNet} \
+#         --root=./outputs \
+#         --video=${video_name} \
+#         --save-dir=output
 
 
 # Generate deliberative
+echo "\n######## Generate video with detected pedestrians ########"
 python3 ./utils/generate_video.py \
-        ${save_folder} ${pedestrians_folder}/detected_pedestrians.csv ./
+        outputs/$2/images outputs/$2/yolov3_outputs/query_list.csv utils/id2rgb.txt ./outputs/$2
--- project/milestone3/utils/generate_video.py
+++ project/milestone3/utils/generate_video.py
@@ -13,24 +13,32 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("input_images", type=str, help="path to images folder")
     parser.add_argument("input_csv", type=str, help="path to csv")
+    parser.add_argument("id2rgb", type=str, help="path to id-to-rgb correspondence tsv.")
     parser.add_argument("output", type=str, help="path")
     args = parser.parse_args()
-    print(args)
+    # print(args)
 
     # read csv
     df = pd.read_csv(args.input_csv)
     images_list = Path(args.input_images) / "image_%04d.png"
 
-    # VideoCapture を作成する。
+    # VideoCapture 
     cap = cv2.VideoCapture(str(images_list))
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = 30
 
-    # VideoWriter を作成する。
+    # VideoWriter 
     fourcc = cv2.VideoWriter_fourcc(*"DIVX")
     writer = cv2.VideoWriter(os.path.join(args.output, 'output.mp4'), fourcc, fps, (width, height))
     image_id = 0
+    
+    # color list
+    df_colorlist = pd.read_csv(args.id2rgb, sep='\t', header=None, index_col=0, names=['rgb'])
+    df_colorlist.rgb = df_colorlist.rgb.apply(lambda x:[int(val) for val in x[1:-1].split(',')])
+
+    color_list = df_colorlist.rgb.values
+    
     while True:
         ret, frame = cap.read()
         if not ret:
@@ -38,8 +46,8 @@ if __name__ == "__main__":
         for box_info in df[df['image_id']==image_id][['x', 'y', 'w', 'h', 'confidence', 'ID']].values:
             [x, y, w, h, conf, ID] = box_info
             x1, y1, x2, y2 = int(x - w/2), int(y - h/2), int(x + w/2), int(y + h/2)
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 220, 0), 3)
-            cv2.putText(frame, 'ID:%d' % ID, (x1, y1), cv2.FONT_HERSHEY_PLAIN, 2.0, (0, 220, 0), 2, cv2.LINE_AA)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), color_list[int(ID%len(color_list))], 3)
+            cv2.putText(frame, 'ID:%d' % ID, (x1, y1), cv2.FONT_HERSHEY_PLAIN, 2.0, color_list[int(ID%len(color_list))], 2, cv2.LINE_AA)
         writer.write(frame)  
         image_id += 1
 
